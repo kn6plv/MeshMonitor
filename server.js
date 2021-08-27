a48bd8a4b1340b1900a11427465717e1522e765e
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-08-26 17:11:34 -0700

    DB testing

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 #! /usr/bin/env node
 
+const Log = require('debug')('main');
 const OLSR = require('./olsrlib');
+const DB = require('./db');
 
 const dev = new OLSR();
 dev.open();
-dev.on('message', m => m.type == 'NS' && console.log(JSON.stringify(m, null, 2)));
+dev.on('message', m => {
+  Log(JSON.stringify(m, null, 2));
+  DB.addMessage(m);
+  console.log(`valid ${DB.validCount()} invalid ${DB.invalidCount()} duplicate ${DB.duplicateCount()} outOfOrder ${DB.outOfOrderCount()}`);
+});
+DB.setMessageTrim(10);
