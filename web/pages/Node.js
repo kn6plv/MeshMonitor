a3d5126c58f87358884e5212eacf3bfe979fc41e
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-09-01 11:31:42 -0700

    More flexible range handling

--- web/pages/Node.js
+++ web/pages/Node.js
@@ -4,10 +4,13 @@ const Rainbow = require('rainbowvis.js');
 const NameService = require('../../nameService');
 const Moment = require('moment');
 
+const HOUR1 = 60 * 60 * 1000;
+
 const TIME = 61 * 60;
 const SCRUB_RANGE = 1007;
 const SCRUB_STEP = 10 * 60;
 const STEP = 10;
+const DECIMATION = 500;
 
 const graphUpdateCache = {};
 let graphId = 1;
@@ -29,13 +32,25 @@ class Node extends Page {
     const config = {
       id: `node_${++graphId}`,
       originator: sortedNames[0].address,
-      start: Date.now() - TIME * 1000
+      start: Date.now() - TIME * 1000,
+      duration: TIME * 1000
     };
 
     if (arg && arg.address) {
+      config.originator = arg.address;
       const selected = sortedNames.find(name => name.address === arg.address);
-      if (selected) {
-        config.originator = selected.address;
+      if (!selected) {
+        sortedNames.unshift({
+          originator: arg.address,
+          address: arg.address,
+          name: arg.address
+        });
+      }
+      if (arg.timestamp) {
+        config.start = HOUR1 * Math.floor(arg.timestamp / HOUR1) - HOUR1;
+      }
+      if (arg.duration) {
+        config.duration = arg.duration * 1000;
       }
     }
 
@@ -50,7 +65,7 @@ class Node extends Page {
         let last = 0;
         (await DB.getSequenceNrs(config.originator, from, to)).forEach((point, idx) => {
           if (point.timestamp >= last) {
-            last = point.timestamp + STEP * 1000;
+            last = point.timestamp + config.duration / DECIMATION;
             cache.data.push({ x: point.timestamp, y: point.seqnr });
             cache.color.push(this.gradient.colorAt(point.maxHop));
           }
@@ -72,23 +87,25 @@ class Node extends Page {
           config.start = Date.now() - (TIME + offset * SCRUB_STEP) * 1000;
           this.send(`chart.update.response.${config.id}`, Object.assign(
             { update: false, live: offset === 0 },
-            await gen(config.start, config.start + TIME * 1000)
+            await gen(config.start, config.start + config.duration)
           ));
           break;
         case 'change':
         default:
           config.originator = msg.value.address;
-          config.start = Date.now() - TIME * 1000;
+          const position = SCRUB_RANGE - ((Date.now() - config.start) / 1000 - TIME) / SCRUB_STEP;
           this.html('info', this.template.Node(Object.assign(
-            { id: config.id, selected: config.originator, nodes: this.getSortedNames(), step: STEP },
-            await gen(config.start, Date.now()))
+            { id: config.id, selected: config.originator, nodes: sortedNames, step: STEP, position: position, live: config.start + config.duration >= Date.now() },
+            await gen(config.start, config.start + config.duration))
           ));
           break;
       }
     }
+
+    const position = SCRUB_RANGE - ((Date.now() - config.start) / 1000 - TIME) / SCRUB_STEP;
     this.html('info', this.template.Node(Object.assign(
-      { id: config.id, selected: config.originator, nodes: this.getSortedNames(), step: STEP },
-      await gen(config.start, Date.now()))
+      { id: config.id, selected: config.originator, nodes: sortedNames, step: STEP, position: position, live: config.start + config.duration >= Date.now() },
+      await gen(config.start, config.start + config.duration))
     ));
   }
 
