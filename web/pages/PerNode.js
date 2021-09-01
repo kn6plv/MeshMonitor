5b78972016fdeb2a298081ecfeb9f2a927a41cc7
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-08-31 17:09:54 -0700

    Traffic graphs per node

--- web/pages/PerNode.js
+++ web/pages/PerNode.js
@@ -1,7 +1,6 @@
 const Page = require('./Page');
 const DB = require('../../db');
-
-const graphUpdateCache = {};
+const NameService = require('../../nameService');
 
 class PerNode extends Page {
 
@@ -11,23 +10,32 @@ class PerNode extends Page {
 
   async select() {
     super.select();
-    this.updates = {};
-    this.html('info', this.template.PerNode());
+
+    const step = 10 * 60;
+    const to = Date.now();
+    const from = to - step * 1000;
+    const nodes = {};
+    (await DB.getMessageGroup(from, to)).forEach(entry => {
+      const node = nodes[entry.originator] || (nodes[entry.originator] = { originator: NameService.lookupNameByIP(entry.originator) || entry.originator, valid: 0, duplicate: 0, outOfOrder: 0, maxHop: 0, minHop: Number.MAX_SAFE_INTEGER });
+      node.valid += entry.valid;
+      node.duplicate += entry.duplicate;
+      node.outOfOrder += entry.outOfOrder;
+      node.maxHop = Math.max(node.maxHop, entry.maxHop);
+      node.minHop = Math.min(node.minHop, entry.maxHop);
+    });
+    const sortedNodes = Object.values(nodes).sort((a, b) => a.originator.localeCompare(b.originator, { sensitivity: 'base' }));
+    const datasets = [];
+    [ 'valid', 'duplicate', 'outOfOrder' ].forEach(key => {
+      const data = [];
+      sortedNodes.forEach(node => data.push(node[key] / step));
+      datasets.push({ label: key, data: data });
+    });
+
+    this.html('info', this.template.PerNode({ id: 'nodes', labels: sortedNodes.map(node => node.originator), datasets: datasets }));
   }
 
   async deselect() {
     super.deselect();
-    this.updates = {};
-  }
-
-  async generateChart(config) {
-  }
-
-  async 'chart.update.request' (msg) {
-    const fn = this.updates[msg.value.id];
-    if (fn) {
-      await fn(msg);
-    }
   }
 
 }
