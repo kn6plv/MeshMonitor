b28a8975e2a6c97f1b8a9e85ea9129c963c1684b
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-08-31 22:04:32 -0700

    Date scrubbing

--- web/pages/Node.js
+++ web/pages/Node.js
@@ -2,8 +2,11 @@ const Page = require('./Page');
 const DB = require('../../db');
 const Rainbow = require('rainbowvis.js');
 const NameService = require('../../nameService');
+const Moment = require('moment');
 
 const TIME = 61 * 60;
+const SCRUB_RANGE = 1007;
+const SCRUB_STEP = 10 * 60;
 const STEP = 10;
 
 const graphUpdateCache = {};
@@ -18,24 +21,32 @@ class Node extends Page {
     this.gradient.setSpectrum('green', 'orange', 'red');
   }
 
-  async select() {
+  async select(arg) {
     super.select();
     this.updates = {};
 
+    const sortedNames = this.getSortedNames();
     const config = {
       id: `node_${++graphId}`,
-      originator: this.getSortedNames()[0].address,
+      originator: sortedNames[0].address,
       start: Date.now() - TIME * 1000
     };
 
-    const gen = async (from) => {
+    if (arg && arg.address) {
+      const selected = sortedNames.find(name => name.address === arg.address);
+      if (selected) {
+        config.originator = selected.address;
+      }
+    }
+
+    const gen = async (from, to) => {
       const cache = graphUpdateCache[config.originator] || (graphUpdateCache[config.originator] = {});
-      const to = Date.now();
       if (cache.from !== from || cache.cursor !== to) {
         cache.from = from;
         cache.cursor = to;
         cache.data = [];
         cache.color = [];
+        cache.date = Moment(from).format('MMMM Do YYYY');
         let last = 0;
         (await DB.getSequenceNrs(config.originator, from, to)).forEach((point, idx) => {
           if (point.timestamp >= last) {
@@ -49,16 +60,36 @@ class Node extends Page {
     }
 
     this.updates[config.id] = async (op, msg) => {
-      if (op === 'update') {
-        this.send(`chart.update.response.${config.id}`, await gen(msg.value.cursor));
-      }
-      else {
-        config.originator = msg.value.address;
-        config.start = Date.now() - TIME * 1000;
-        this.html('info', this.template.Node(Object.assign({ id: config.id, selected: config.originator, nodes: this.getSortedNames(), step: STEP }, await gen(config.start))));
+      switch (op) {
+        case 'update':
+          this.send(`chart.update.response.${config.id}`, Object.assign(
+            { update: true, live: true },
+            await gen(msg.value.cursor, Date.now())
+          ));
+          break;
+        case 'position':
+          const offset = SCRUB_RANGE - msg.value.position;
+          config.start = Date.now() - (TIME + offset * SCRUB_STEP) * 1000;
+          this.send(`chart.update.response.${config.id}`, Object.assign(
+            { update: false, live: offset === 0 },
+            await gen(config.start, config.start + TIME * 1000)
+          ));
+          break;
+        case 'change':
+        default:
+          config.originator = msg.value.address;
+          config.start = Date.now() - TIME * 1000;
+          this.html('info', this.template.Node(Object.assign(
+            { id: config.id, selected: config.originator, nodes: this.getSortedNames(), step: STEP },
+            await gen(config.start, Date.now()))
+          ));
+          break;
       }
     }
-    this.html('info', this.template.Node(Object.assign({ id: config.id, selected: config.originator, nodes: this.getSortedNames(), step: STEP }, await gen(config.start))));
+    this.html('info', this.template.Node(Object.assign(
+      { id: config.id, selected: config.originator, nodes: this.getSortedNames(), step: STEP },
+      await gen(config.start, Date.now()))
+    ));
   }
 
   async deselect() {
@@ -77,6 +108,13 @@ class Node extends Page {
     const fn = this.updates[msg.value.id];
     if (fn) {
       await fn('change', msg);
+    }
+  }
+
+  async 'chart.position.change' (msg) {
+    const fn = this.updates[msg.value.id];
+    if (fn) {
+      await fn('position', msg);
     }
   }
 
