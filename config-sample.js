4e2e37ebbd452e28631fac332e809a09bf92fdd6
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-09-03 11:54:07 -0700

    Config sample

---
+++ config-sample.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  General: {
+    Name: 'Bay Area'
+  },
+
+  Twilio: {
+    accountSid: '...',
+    authToken: '...',
+    toPhoneNumber: '+15551231234',
+    fromPhoneNumber: '+15556786789'
+  },
+
+};
