9797da44864dd9214ad7c4767145ae8ac9eb4f4b
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-09-09 11:15:25 -0700

    Comment out Twilio by default

--- config-sample.js
+++ config-sample.js
@@ -5,11 +5,15 @@ module.exports = {
     PortNr: 8083
   },
 
+  /* Uncomment and complete the Twilio section if you want alerts
+
   Twilio: {
     accountSid: '...',
     authToken: '...',
     toPhoneNumber: '+15551231234',
     fromPhoneNumber: '+15556786789'
   },
+
+  */
 
 };
