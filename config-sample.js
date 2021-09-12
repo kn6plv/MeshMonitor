d3839fd141979cf00f2b000b3c44e0d8e884c19c
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-09-11 17:48:26 -0700

    Update config sample

--- config-sample.js
+++ config-sample.js
@@ -5,6 +5,15 @@ module.exports = {
     PortNr: 8083
   },
 
+  Health: {
+    Storm: {
+      HopCount: {
+        Begin: 50,
+        End: 40
+      }
+    }
+  },
+
   /* Uncomment and complete the Twilio section if you want alerts
 
   Twilio: {
